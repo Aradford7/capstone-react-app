0a13467c88dbc306293f5c115b546e90eeda2a0b
Ashley Radford <aradford7@outlook.com>
2019-06-08 19:13:37 -0700

    start userReducer

--- src/Redux/Actions/userActions.js
+++ src/Redux/Actions/userActions.js
@@ -1,23 +1,25 @@
 
 import {SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI} from '../types';
 
-export const loginUser = (userData) => (dispatch) => {
+export const loginUser = (userData, history) => (dispatch) => {
     dispatch({type: LOADING_UI});
     axios
         .post('/login' , userData)
         .then((res) => {
             const FBIdToken = `Bearer ${res.data.token}`;
             localStorage.setItem('FBIDToken', FBIdToken);
-            axios.defaults.headers.common['Authorization'] = FBIdToken
-            this.props.history.push('/codey');
+            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            dispatch(getUserData());
+            dispatch({type: CLEAR_ERRORS});
+            history.push('/codey');
             })
             .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
+                dispatch({
+                    type: SET_ERRORS,
+                    payload: err.response.data
                 });
             });
-}
+    }
 
 export const getUserData = () => (dispatch) => {
    axios.get('/user')
