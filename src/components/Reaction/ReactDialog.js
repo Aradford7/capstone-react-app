dbc4c58015e3f14272f4ea04c2f49fe65254b06f
Ashley Radford <aradford7@outlook.com>
2019-06-10 00:26:36 -0700

    fix

--- src/components/Reaction/ReactDialog.js
+++ src/components/Reaction/ReactDialog.js
@@ -19,6 +19,28 @@ const styles = {
     invisibleSeparator: {
         border: 'none',
         margin: 4
+    },
+    profileImage: {
+        maxWidth: 200,
+        height: 200,
+        borderRadius: '50%',
+        objectFit : 'cover'
+    },
+    dialogContent: {
+        padding: 20
+    },
+    closeButton: {
+        position: 'absolute',
+        left: '90%'
+    },
+    // expandButton: {
+    //     position: 'absolute',
+    //     left: '90%'
+    // }
+    spinnerDiv: {
+        textAlign: 'center',
+        marginTop: 50,
+        marginBottom: 50
     }
 
 }
